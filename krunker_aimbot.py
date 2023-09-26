c7277c608360e40245c02fd06fe85192e9c944bd
Victor Joulin-Batejat <78279041+VictorJB24@users.noreply.github.com>
2023-09-25 23:19:36 -0700

    Ability to screenshot, formatting code better

--- krunker_aimbot.py
+++ krunker_aimbot.py
@@ -1,7 +1,15 @@
+
+"""
+    Description: ...
+    Authors: Victor J. & Aiden A. that rhymes
+    Date: Summer 2023
+"""
+
 import numpy as np
 import cv2
 from mss import mss
 from PIL import Image
+from datetime import datetime
 
 bounding_box = {'top': 100, 'left': 0, 'width': 400, 'height': 300}
 
@@ -9,11 +17,15 @@ screenshotter = mss()
 
 while True:
     sct_img = screenshotter.grab(bounding_box)
-    cv2.imshow('screen', np.array(sct_img))
+    cv2.imshow('Krunker Window', np.array(sct_img))
 
     if (cv2.waitKey(1) & 0xFF) == ord('q'):
         cv2.destroyAllWindows()
         break
+
+    if (cv2.waitKey(1) & 0xFF) == ord('p'):
+        now = datetime.now().strftime("%H:%M:%S")
+        cv2.imwrite(now+".jpg", np.array(sct_img))
 
 def dimensions_check():
     pass
