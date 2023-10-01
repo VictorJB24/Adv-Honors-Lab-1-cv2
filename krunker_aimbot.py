50b3417ab45110f3b2db3ea8741d027d8f73262c
Victor Joulin-Batejat <78279041+VictorJB24@users.noreply.github.com>
2023-09-30 23:11:25 -0700

    Trying to add key detection for autoaim

--- krunker_aimbot.py
+++ krunker_aimbot.py
@@ -13,8 +13,13 @@ from PIL import Image
 from datetime import datetime
 import pyautogui
 from nametag_detection import get_enemey_coords
+from pynput.keyboard import Key, Listener
 
 pyautogui.FAILSAFE = True
+
+# need to declare global to avoid UnboundLocalError from on_press
+global AUTO_AIM_ON
+AUTO_AIM_ON = False
 
 ap = argparse.ArgumentParser()
 ap.add_argument("-c", "--coordinates",
@@ -41,6 +46,14 @@ def calc_coordinates():
 
     return top, left, krunker_window_width, krunker_window_height
 
+def on_press(key):
+    try:
+        if key.char == ('a'):
+            print("autoaim", AUTO_AIM_ON)
+            AUTO_AIM_ON = not AUTO_AIM_ON
+    except AttributeError:
+        return
+
 def main():
     """
     if not args["coordinates"]:
@@ -51,41 +64,40 @@ def main():
     screen_width, screen_height = pyautogui.size()
     bounding_box = {'top': 100, 'left': 0, 'width': screen_width, 'height': screen_height-100}
 
-    AUTO_AIM_ON = True
+    with Listener(on_press=on_press) as listener:
+        listener.join()
 
-    while True:
+        while True:
 
-        scrt_img = screenshotter.grab(bounding_box)
+            if AUTO_AIM_ON:
+                scrt_img = screenshotter.grab(bounding_box)
 
-        krunker_frame = np.array(scrt_img)
-        # cv2.imshow('Krunker Window', krunker_frame)
+                krunker_frame = np.array(scrt_img)
+                # cv2.imshow('Krunker Window', krunker_frame)
 
-        # Masks arm to prevent accidental sleeve detection
-        mask = np.zeros(krunker_frame.shape[:2], dtype = "uint8")
-        (cX, cY) = (krunker_frame.shape[1] // 2, krunker_frame.shape[0] // 2)
-        cv2.rectangle(mask, (400, 400), (krunker_frame.shape[1] - 400, krunker_frame.shape[0] - 400), 255, -1)
-        cv2.rectangle(mask, (cX -75, cY + 250), (cX + 500, krunker_frame.shape[0]), 0, -1)
-        masked_image = cv2.bitwise_and(krunker_frame, krunker_frame, mask = mask)
+                # Masks arm to prevent accidental sleeve detection
+                mask = np.zeros(krunker_frame.shape[:2], dtype = "uint8")
+                (cX, cY) = (krunker_frame.shape[1] // 2, krunker_frame.shape[0] // 2)
+                cv2.rectangle(mask, (400, 400), (krunker_frame.shape[1] - 400, krunker_frame.shape[0] - 400), 255, -1)
+                cv2.rectangle(mask, (cX -75, cY + 250), (cX + 500, krunker_frame.shape[0]), 0, -1)
+                masked_image = cv2.bitwise_and(krunker_frame, krunker_frame, mask = mask)
 
-        cursor_coords = get_enemey_coords(masked_image)
+                cursor_coords = get_enemey_coords(masked_image)
 
-        if not cursor_coords:
-            continue
+                if not cursor_coords:
+                    continue
 
-        # pyautogui.dragTo(cursor_coords[0], cursor_coords[1], duration=.001)  # drag mouse to XY
-        pyautogui.tripleClick(x=cursor_coords[0], y=cursor_coords[1])
+                # pyautogui.dragTo(cursor_coords[0], cursor_coords[1], duration=.001)  # drag mouse to XY
+                pyautogui.tripleClick(x=cursor_coords[0], y=cursor_coords[1])
 
-        if (cv2.waitKey(1) & 0xFF) == ord('q'):
-            cv2.destroyAllWindows()
-            break
-
-        if (cv2.waitKey(1) & 0xFF) == ord('p'):
-            now = datetime.now().strftime("%H:%M:%S")
-            cv2.imwrite(now+".jpg", np.array(sct_img))
-
-        """
-        if (cv2.waitKey(1) & 0xFF) == ord('a'):
-            AUTO_AIM_ON = not AUTO_AIM_ON
-        """
 
 main()
+
+if __name__ == "__main__":
+
+    if (cv2.waitKey(1) & 0xFF) == ord('q'):
+        cv2.destroyAllWindows()
+
+    if (cv2.waitKey(1) & 0xFF) == ord('p'):
+        now = datetime.now().strftime("%H:%M:%S")
+        cv2.imwrite(now+".jpg", np.array(sct_img))
